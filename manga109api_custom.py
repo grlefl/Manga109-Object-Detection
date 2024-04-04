4fc4b4f30ea8454c085a75f279c58514a0807bb3
G.L. Flores <124198528+grlefl@users.noreply.github.com>
2024-04-04 16:17:01 -0500

    ummm what did I change

---
+++ manga109api_custom.py
@@ -0,0 +1,17 @@
+import manga109api
+
+
+class Parser:
+    def __init__(self, root_dir):
+        self.parser = manga109api.Parser(root_dir=root_dir)
+
+    def load_all_images(self):
+        images = []
+        for book_title in self.parser.books:
+            book_annotation = self.parser.get_annotation(book=book_title)
+            pages = book_annotation["page"]
+            for page_annotation in pages:
+                page_index = page_annotation["@index"]
+                img_path = self.parser.img_path(book=book_title, index=page_index)
+                images.append({"img_path": img_path, "page_annotation": page_annotation})
+        return images
---
+++ visualize_data.py
@@ -0,0 +1,22 @@
+from PIL import ImageDraw, Image
+
+
+def draw_rectangle(image, x0, y0, x1, y1, annotation_type):
+    assert annotation_type in ["body", "face", "frame", "text"]
+
+    color = {"body": "#258039", "face": "#f5be41",
+             "frame": "#31a9b8", "text": "#cf3721"}[annotation_type]
+    draw = ImageDraw.Draw(image)
+    draw.rectangle((x0, y0, x1, y1), outline=color, width=10)
+
+
+def draw_annotations(img_path, page_annotation):
+    image = Image.open(img_path)
+
+    for annotation_type in ["body", "face", "frame", "text"]:
+        rois = page_annotation[annotation_type]
+        # regions of interest
+        for roi in rois:
+            draw_rectangle(image, int(roi["@xmin"]), int(roi["@ymin"]), int(roi["@xmax"]), int(roi["@ymax"]),
+                           annotation_type)
+    return image
