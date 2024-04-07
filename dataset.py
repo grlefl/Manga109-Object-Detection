773cc0983af031186a22c259ea3201a325cf0f5f
G.L. Flores <124198528+grlefl@users.noreply.github.com>
2024-04-07 13:44:42 -0500

    need to redo everything

--- dataset.py
+++ dataset.py
@@ -1,5 +1,7 @@
+import numpy as np
 import torch
 from PIL import Image
+from matplotlib import pyplot as plt, patches
 from torch.utils.data import Dataset
 
 
@@ -7,13 +9,14 @@ class CustomDataset(Dataset):
     def __init__(self, img_dict, transform=None):
         self.img_dict = img_dict
         self.transform = transform
+        self.label_mapping = {'face': 1, 'text': 2, 'frame': 3, 'body': 4}
 
     def __len__(self):
         return len(self.img_dict)
 
     def __getitem__(self, idx):
         img_path = self.img_dict[idx]['img_path']
-        annotations = self.img_dict[idx]['page_annotation']
+        page_annotation = self.img_dict[idx]['page_annotation']
 
         image = Image.open(img_path).convert("RGB")
 
@@ -22,19 +25,22 @@ class CustomDataset(Dataset):
         areas = []
         iscrowd = []
 
-        for label in annotations:
-            xmin = label['@xmin']
-            ymin = label['@ymin']
-            xmax = label['@xmax']
-            ymax = label['@ymax']
+        valid_categories = ['face', 'text', 'frame', 'body']
+        for category in valid_categories:
+            if category in page_annotation:
+                for box in page_annotation[category]:
+                    xmin = box['@xmin']
+                    ymin = box['@ymin']
+                    xmax = box['@xmax']
+                    ymax = box['@ymax']
 
-            bboxes.append([xmin, ymin, xmax, ymax])
-            labels.append(label)  # Assuming the category is the label itself
-            areas.append((xmax - xmin) * (ymax - ymin))
-            iscrowd.append(0)  # Assuming all annotations are single instances, not crowds
+                    bboxes.append([xmin, ymin, xmax, ymax])
+                    labels.append(self.label_mapping[category])  # Assuming the category is the label itself
+                    areas.append((xmax - xmin) * (ymax - ymin))
+                    iscrowd.append(0)  # Assuming all annotations are single instances, not crowds
 
         # Convert lists to tensors
-        bboxes = torch.as_tensor(boxes, dtype=torch.float32)
+        bboxes = torch.as_tensor(bboxes, dtype=torch.float32)
         labels = torch.tensor(labels, dtype=torch.int64)
         image_id = torch.tensor([idx], dtype=torch.int64)
         areas = torch.as_tensor(areas, dtype=torch.float32)
@@ -49,6 +55,25 @@ class CustomDataset(Dataset):
         }
 
         if self.transform:
-            image, my_annotation = self.transform(image=image, bboxes=bboxes)
+            image_np = np.array(image)
+            image, my_annotation = self.transform(image=image_np, bboxes=bboxes)
 
         return image, my_annotation
+
+
+def visualize_image_with_boxes(image, target):
+    fig, ax = plt.subplots(1)
+    ax.imshow(image)
+
+    boxes = target["boxes"]
+    labels = target["labels"]
+
+    for box, label in zip(boxes, labels):
+        xmin, ymin, xmax, ymax = box
+        width = xmax - xmin
+        height = ymax - ymin
+        rect = patches.Rectangle((xmin, ymin), width, height, edgecolor='r', facecolor='none')
+        ax.add_patch(rect)
+        ax.text(xmin, ymin, f'Label: {label}', color='r')
+
+    plt.show()
