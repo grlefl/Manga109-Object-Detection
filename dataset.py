d473da5672875059db70d6179012ef239262f464
G.L. Flores <124198528+grlefl@users.noreply.github.com>
2024-04-07 18:03:37 -0500

    dataset is gud

--- dataset.py
+++ dataset.py
@@ -1,4 +1,4 @@
-from PIL import Image
+import cv2
 from torch.utils.data import Dataset
 
 
@@ -12,13 +12,13 @@ class CustomDataset(Dataset):
 
     def __getitem__(self, idx):
         img_path = self.img_df.iloc[idx]['img_path']
-        bboxes = self.img_df.iloc[idx]['bboxes']
-        labels = self.img_df.iloc[idx]['labels']
+        bboxes = self.img_df.iloc[idx]['bboxes']        # [[box#1], [box#2], [box#3]]
+        labels = self.img_df.iloc[idx]['labels']        # [label#1, label#2, label#3]
 
-        # load the image using PIL
-        image = Image.open(img_path).convert('RGB')
+        image = cv2.imread(img_path)                            # load the image as numpy array (BGR format)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)          # convert BGR image to RGB
 
         if self.transform:
-            image, bboxes, labels = self.transform(image=image, bboxes=bboxes, class_labels=labels)
+            image, bboxes, labels = self.transform(image=image, bboxes=bboxes, labels=labels)
 
         return image, bboxes, labels
