577cddf6808bb7bb0a84d139b9351f0500b79d40
G.L. Flores <124198528+grlefl@users.noreply.github.com>
2024-04-06 23:40:58 -0500

    no progress here

--- dataset.py
+++ dataset.py
@@ -3,20 +3,20 @@ import torch
 from torch.utils.data import Dataset
 from torchvision.datasets.folder import pil_loader
 
-""" Custom class used to create the training and validation sets. """ # taken from github
-class CustomDataset(Dataset):
+
+class CustomDataset(Dataset):       # taken from github!!!
     # initialize configurations
     def __init__(self, images, width, height, classes, transforms=None):
 
-        self.transforms=transforms
+        self.transforms = transforms
         self.images = images
         self.height = height
         self.width = width
         self.classes = classes
 
-        self.image_paths = images["path"].to_list()
+        self.image_paths = images["img_path"].to_list()
         self.image_annotations = images["page_annotation"]
-        self.all_images = ["".join(path.split(os.path.sep)[-2:]) for path in self.image_paths]
+        self.all_images = ["".join(path.split(os.path.sep)[-2:]) for path in self.image_paths] # okay for now
 
     def __len__(self):
         return len(self.all_images)
@@ -30,7 +30,7 @@ class CustomDataset(Dataset):
 
         boxes = []
         labels = []
-        authors = []
+        #authors = []
 
         image_width, image_height = image.size
 
@@ -39,7 +39,7 @@ class CustomDataset(Dataset):
             rois = self.image_annotations[idx][annotation_type]
             for roi in rois:
                 labels.append(self.classes.index(annotation_type))
-                authors.append(roi["author"])
+                #authors.append(roi["author"])
                 # xmin = left corner x-coordinates
                 xmin = roi["@xmin"]
                 # xmax = right corner x-coordinates
@@ -61,7 +61,7 @@ class CustomDataset(Dataset):
         area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])    # area of the bounding boxes
         iscrowd = torch.zeros((boxes.shape[0],), dtype=torch.int64)         # no crowd instances
         labels = torch.as_tensor(labels, dtype=torch.int64)                 # labels to tensor
-        authors = torch.as_tensor(authors, dtype=torch.int64)
+        #authors = torch.as_tensor(authors, dtype=torch.int64)
 
         # prepare the final dictionary
         target = {"boxes": boxes, "labels": labels, "area": area, "author": authors, "iscrowd": iscrowd}
