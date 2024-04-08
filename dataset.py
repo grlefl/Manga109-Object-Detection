155758f7abee73aa2ec067c3a122477bc821682a
G.L. Flores <124198528+grlefl@users.noreply.github.com>
2024-04-08 08:37:58 -0500

    new rough draft

--- dataset.py
+++ dataset.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 import torch
 from torch.utils.data import Dataset
 
@@ -17,29 +18,29 @@ class CustomDataset(Dataset):
         bboxes = self.img_df.iloc[idx]['bboxes']        # [[box#1], [box#2], [box#3]]
         labels = self.img_df.iloc[idx]['labels']        # [label#1, label#2, label#3]
 
-        image = cv2.imread(img_path)                        # load the image as numpy array (BGR format)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)      # convert BGR image to RGB format
-        # image = image.astype(np.float32) / 255.             # normalize image for pixel range [0, 1]
+        image = cv2.imread(img_path)                                        # load the image as numpy array (BGR format)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)   # convert BGR image to RGB format
+        # image = image.astype(np.float32) / 255.                           # normalize image for pixel range [0, 1]
 
-        boxes = torch.as_tensor(bboxes, dtype=torch.float32)                # bounding box to tensor
-        # area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])    # area of the bounding boxes
-        # iscrowd = torch.zeros((boxes.shape[0],), dtype=torch.int64)         # no crowd instances
-        labels = torch.as_tensor(labels, dtype=torch.int64)                 # labels to tensor
+        bboxes = torch.as_tensor(bboxes, dtype=torch.float32)                        # bounding box to tensor
+        # area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])          # area of the bounding boxes
+        # iscrowd = torch.zeros((boxes.shape[0],), dtype=torch.int64)               # no crowd instances
+        labels = torch.as_tensor(labels, dtype=torch.int64)                         # labels to tensor
 
         # prepare the final `target` dictionary
         target = {}
-        target["boxes"] = boxes
+        target["bboxes"] = bboxes
         target["labels"] = labels
-        target["area"] = area
-        target["iscrowd"] = iscrowd
-        image_id = torch.tensor([idx])
-        target["image_id"] = image_id
+        # target["area"] = area
+        # target["iscrowd"] = iscrowd
+        # image_id = torch.tensor([idx])
+        # target["image_id"] = image_id
 
         # apply the image transforms
         if self.transform:
-            transformed = self.transform(image=image, bboxes=target['boxes'], labels=labels)
+            transformed = self.transform(image=image, bboxes=target['bboxes'], labels=labels)
             image = transformed['image']
-            target['boxes'] = torch.Tensor(transformed['bboxes'])
+            target['bboxes'] = torch.Tensor(transformed['bboxes'])
 
         return image, target
 
