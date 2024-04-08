98158ef3893b43e3e7d97dff8fdd243be949767a
G.L. Flores <124198528+grlefl@users.noreply.github.com>
2024-04-08 09:42:19 -0500

    averager function?

---
+++ utils.py
@@ -0,0 +1,21 @@
+# this class keeps track of the training and validation loss values...
+# ... and helps to get the average for each epoch as well
+class Averager:
+    def __init__(self):
+        self.current_total = 0.0
+        self.iterations = 0.0
+
+    def send(self, value):
+        self.current_total += value
+        self.iterations += 1
+
+    @property
+    def value(self):
+        if self.iterations == 0:
+            return 0
+        else:
+            return 1.0 * self.current_total / self.iterations
+
+    def reset(self):
+        self.current_total = 0.0
+        self.iterations = 0.0
