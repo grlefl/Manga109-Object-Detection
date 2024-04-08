a4a69157458549de5e9ca1164c1c87ee0bc81b46
G.L. Flores <124198528+grlefl@users.noreply.github.com>
2024-04-08 08:43:00 -0500

    visualize data rough draft

--- visualize_data.py
+++ visualize_data.py
@@ -1,22 +1,26 @@
-from PIL import ImageDraw, Image
-
-
-def draw_rectangle(image, x0, y0, x1, y1, annotation_type):
-    assert annotation_type in ["body", "face", "frame", "text"]
-
-    color = {"body": "#258039", "face": "#f5be41",
-             "frame": "#31a9b8", "text": "#cf3721"}[annotation_type]
-    draw = ImageDraw.Draw(image)
-    draw.rectangle((x0, y0, x1, y1), outline=color, width=10)
-
-
-def draw_annotations(image, page_annotation):
-    # image = Image.open(img_path)
-
-    for annotation_type in ["body", "face", "frame", "text"]:
-        rois = page_annotation[annotation_type]
-        # regions of interest
-        for roi in rois:
-            draw_rectangle(image, int(roi["@xmin"]), int(roi["@ymin"]), int(roi["@xmax"]), int(roi["@ymax"]),
-                           annotation_type)
-    return image
+def show_tranformed_image(train_loader):
+    """
+    This function shows the transformed images from the `train_loader`.
+    Helps to check whether the tranformed images along with the corresponding
+    labels are correct or not.
+    Only runs if `VISUALIZE_TRANSFORMED_IMAGES = True` in config.py.
+    """
+    if len(train_loader) > 0:
+        for i in range(1):
+            images, targets = next(iter(train_loader))
+            images = list(image.to(DEVICE) for image in images)
+            targets = [{k: v.to(DEVICE) for k, v in t.items()} for t in targets]
+            boxes = targets[i]['boxes'].cpu().numpy().astype(np.int32)
+            labels = targets[i]['labels'].cpu().numpy().astype(np.int32)
+            sample = images[i].permute(1, 2, 0).cpu().numpy()
+            for box_num, box in enumerate(boxes):
+                cv2.rectangle(sample,
+                            (box[0], box[1]),
+                            (box[2], box[3]),
+                            (0, 0, 255), 2)
+                cv2.putText(sample, CLASSES[labels[box_num]],
+                            (box[0], box[1]-10), cv2.FONT_HERSHEY_SIMPLEX,
+                            1.0, (0, 0, 255), 2)
+            cv2.imshow('Transformed image', sample)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
