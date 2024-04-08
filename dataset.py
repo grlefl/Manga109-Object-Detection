9599720c94799f7e5806194cf4c6d8537d0a461b
G.L. Flores <124198528+grlefl@users.noreply.github.com>
2024-04-07 21:29:40 -0500

    things are... okay
    rgb images seem to be scaling and displaying okay

--- dataset.py
+++ dataset.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 from torch.utils.data import Dataset
 
 
@@ -15,13 +16,14 @@ class CustomDataset(Dataset):
         bboxes = self.img_df.iloc[idx]['bboxes']        # [[box#1], [box#2], [box#3]]
         labels = self.img_df.iloc[idx]['labels']        # [label#1, label#2, label#3]
 
-        image = cv2.imread(img_path)                            # load the image as numpy array (BGR format)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)          # convert BGR image to RGB
+        image = cv2.imread(img_path)                        # load the image as numpy array (BGR format)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)      # convert BGR image to RGB format
+        # image = image.astype(np.float32) / 255.             # normalize image for pixel range [0, 1]
 
         if self.transform:
-            transformed = self.transform(image=image, bboxes=bboxes, labels=labels)
+            transformed = self.transform(image=image)  # , bboxes=bboxes, labels=labels
             image = transformed['image']
-            bboxes = transformed['bboxes']
-            labels = transformed['labels']
+            # bboxes = transformed['bboxes']
+            # labels = transformed['labels']
 
-        return image, bboxes, labels
+        return image    #, bboxes, labels
