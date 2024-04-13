fc78b36828c6da774a197512e5926a948b81cba7
glflores <grace-flores@uiowa.edu>
2024-04-13 17:08:20 -0500

    trying to figure out how to have no bounding box on an image

--- dataset.py
+++ dataset.py
@@ -14,24 +14,27 @@ class CustomDataset(Dataset):
 
     def __getitem__(self, idx):
         img_path = self.img_df.iloc[idx]['img_path']
-        boxes = self.img_df.iloc[idx]['bboxes']         # [[box#1], [box#2], [box#3]]
+        bboxes = self.img_df.iloc[idx]['bboxes']         # [[box#1], [box#2], [box#3]]
         labels = self.img_df.iloc[idx]['labels']        # [label#1, label#2, label#3]
 
         image = cv2.imread(img_path)                            # load the image as numpy array (BGR format)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)          # convert BGR image to RGB format
         image = image.astype(np.float32) / 255.                 # normalize image for pixel range [0, 1]
 
-        # prepare the final `target` dictionary
         target = {}
-        target["boxes"] = boxes
-        target["labels"] = labels
 
         # apply the image transforms
         if self.transform:
-            transformed = self.transform(image=image, bboxes=target['boxes'], labels=target['labels'])
+            transformed = self.transform(image=image, bboxes=bboxes, labels=labels)
+
+            # placeholder for if all bboxes are removed
+            if len(transformed['bboxes']) == 0:
+                print('GOTCHA')
+                transformed['bboxes'].append([0, 0, 0, 0])
+
             image = transformed['image']
-            target['boxes'] = torch.tensor(transformed['bboxes'], dtype=torch.float32)  # format to tensor
-            target['labels'] = torch.tensor(transformed['labels'], dtype=torch.int64)   # format to tensor
+            target = {'boxes': torch.tensor(transformed['bboxes'], dtype=torch.float32),
+                      'labels': torch.tensor(transformed['labels'], dtype=torch.int64)}
 
         return image, target
     
