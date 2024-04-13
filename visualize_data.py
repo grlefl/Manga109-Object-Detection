090d1af88cd96abafbd3bb55c2eb513dd4740eea
G.L. Flores <124198528+grlefl@users.noreply.github.com>
2024-04-13 13:54:29 -0500

    fixed label colors

--- visualize_data.py
+++ visualize_data.py
@@ -23,10 +23,6 @@ def visualize_images(data_loader, label_colors, num_images_to_visualize=3):
                           (int(box[0]), int(box[1])),
                           (int(box[2]), int(box[3])),
                           color, 2)
-            cv2.putText(image, class_name,
-                        (int(box[0]), int(box[1] - 5)),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, color,
-                        2, lineType=cv2.LINE_AA)
 
         axes[count].imshow(image)
         axes[count].axis('off')
