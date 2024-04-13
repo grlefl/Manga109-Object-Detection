82494f120e56e6d630879fd173489d0432ce22e0
G.L. Flores <124198528+grlefl@users.noreply.github.com>
2024-04-13 13:36:52 -0500

    new function for visualizing data

--- visualize_data.py
+++ visualize_data.py
@@ -1,26 +1,38 @@
-def show_tranformed_image(train_loader):
-    """
-    This function shows the transformed images from the `train_loader`.
-    Helps to check whether the tranformed images along with the corresponding
-    labels are correct or not.
-    Only runs if `VISUALIZE_TRANSFORMED_IMAGES = True` in config.py.
-    """
-    if len(train_loader) > 0:
-        for i in range(1):
-            images, targets = next(iter(train_loader))
-            images = list(image.to(DEVICE) for image in images)
-            targets = [{k: v.to(DEVICE) for k, v in t.items()} for t in targets]
-            boxes = targets[i]['boxes'].cpu().numpy().astype(np.int32)
-            labels = targets[i]['labels'].cpu().numpy().astype(np.int32)
-            sample = images[i].permute(1, 2, 0).cpu().numpy()
-            for box_num, box in enumerate(boxes):
-                cv2.rectangle(sample,
-                            (box[0], box[1]),
-                            (box[2], box[3]),
-                            (0, 0, 255), 2)
-                cv2.putText(sample, CLASSES[labels[box_num]],
-                            (box[0], box[1]-10), cv2.FONT_HERSHEY_SIMPLEX,
-                            1.0, (0, 0, 255), 2)
-            cv2.imshow('Transformed image', sample)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def visualize_images(data_loader, label_colors, num_images_to_visualize=3):
+    count = 0
+
+    fig, axes = plt.subplots(1, num_images_to_visualize, figsize=(15, 5))
+
+    for image, target in data_loader:
+        image = np.transpose(np.asarray(image[1]), (1, 2, 0))
+        image = (image * 255).astype(np.uint8)
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        bboxes = np.array(target[1]['boxes'])
+        labels = np.array(target[1]['labels'])
+
+        for i, box in enumerate(bboxes):
+            class_name = str(labels[i])
+            color = label_colors.get(class_name, (255, 255, 255))
+            cv2.rectangle(image,
+                          (int(box[0]), int(box[1])),
+                          (int(box[2]), int(box[3])),
+                          color, 2)
+            cv2.putText(image, class_name,
+                        (int(box[0]), int(box[1] - 5)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, color,
+                        2, lineType=cv2.LINE_AA)
+
+        axes[count].imshow(image)
+        axes[count].axis('off')
+        count += 1
+
+        if count == num_images_to_visualize:
+            break
+
+    plt.show()
